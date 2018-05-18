0421e2c5510fc0cd82b65493e41e234b8fff90e2
wangkungit <914884939@qq.com>
2018-05-18 13:58:49 +0800

    提交了一个项目

---
+++ project/js/login.js
@@ -0,0 +1,26 @@
+function addCookie(key,value,day){
+	var date=new Date();//创建日期对象
+	date.setDate(date.getDate()+day);//过期时间：获取当前的日期+天数，设置给date
+	document.cookie=key+'='+encodeURI(value)+';expires='+date;//添加cookie，设置过期时间
+}
+$('#btn').on('click',function(){
+	var $username=$('#tel').val();//用户名
+	var $password=$('#password').val();//密码
+	$.ajax({
+		type:'post',
+		url:'http://127.0.0.1/project/php/login.php?__hbt=1526545120709',
+		data:{//将用户名和密码传输给后端
+			tel:$username,
+			pass:$password
+		},
+		success:function(data){//请求成功，接收后端返回的值
+			if(!data){//用户名或者密码错误
+				$('#password').val('');
+				$('.msg_error').show();
+			}else{//成功
+				addCookie('UserName',$username,7);
+				location.href='../html/index.html';
+			}
+		}
+	})
+});
