15def809f5bb57a782252ac6bb74db7a25d15977
wangkungit <914884939@qq.com>
2018-05-20 17:24:25 +0800

    123

--- project/php/conn.php
+++
@@ -1,9 +0,0 @@
-<?php 
-	header('content-type:text/html;charset="utf-8"');
-	$conn=@mysql_connect('localhost','root','');
-	if(!$conn){
-		die('数据库连接有问题:'.mysql_error());
-	}
-	mysql_select_db('juanpi');
-	mysql_query('SET NAMES UTF8');
-?>
